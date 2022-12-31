2961755aeffe8e19c458c68d56d92954b6cb0217
graphoeil <graphoeil@gmail.com>
2022-12-31 12:57:24 +0100

    All routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,15 +1,27 @@
 // // Get all the tasks
 const getAllTasks = (req, res) => {
-	res.send('All items from the file');
+	res.send('Get all tasks');
 };
 
 // Create a new task
+const createTask = (req, res) => {
+	res.send('Create task');
+};
 
 // Get single task
+const getTask = (req, res) => {
+	res.send('Get single task');
+};
 
 // Update task
+const updateTask = (req, res) => {
+	res.send('Update task');
+};
 
 // Delete task
+const deleteTask = (req, res) => {
+	res.send('Delete task');
+};
 
 // Exports
-module.exports = { getAllTasks };
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,7 @@
 // Imports
 const express = require('express');
-const { getAllTasks } = require('../controllers/tasks');
+const { getAllTasks, createTask, getTask, 
+	updateTask, deleteTask } = require('../controllers/tasks');
 
 // Router
 const router = express.Router();
@@ -10,12 +11,16 @@ const router = express.Router();
 router.get('/', getAllTasks);
 
 // Create a new task
+router.post('/', createTask);
 
 // Get single task
+router.get('/:id', getTask);
 
 // Update task
+router.patch('/:id', updateTask);
 
 // Delete task
+router.delete('/:id', deleteTask);
 
 // Export
 module.exports = router;
