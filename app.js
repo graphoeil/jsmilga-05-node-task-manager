8cd190673741b721f6aab2c1d5900f1a9670cbcd
graphoeil <graphoeil@gmail.com>
2022-12-31 12:28:49 +0100

    Basic express server

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const express = require('express');
 
 // App
 const app = express();
+
+// Routes
+// Homepage
+app.get('/', (req, res) => {
+	res.status(200).send('Task Manager App');
+});
+
+// Listen
+const port = 3000;
+app.listen(port, console.log(`Server is listening on port ${ port }...`));
