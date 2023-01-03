ac767b2dcf30eaa70dab8aa90d85031946202af1
graphoeil <graphoeil@gmail.com>
2023-01-03 14:42:28 +0100

    put vs patch

--- routes/tasks.js
+++ routes/tasks.js
@@ -17,6 +17,15 @@ router.post('/', createTask);
 router.get('/:id', getTask);
 
 // Update task
+// Patch will partially update the old data by replacing element that are submitted in the request
+// Put will replacing the old data with the data submitted in the request, but for that we must add overwrite:true
+// Without overwrite mongoose will still patch the request
+/* const task = await Task.findOneAndUpdate({ _id:taskID }, req.body, {
+	// Options to return new task and for validating data passed
+	new:true,
+	runValidators:true,
+	overwrite:true <= !!!!!!!!!!!!!!!
+}); */
 router.patch('/:id', updateTask);
 
 // Delete task
